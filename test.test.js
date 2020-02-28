c0572eae3bb4b86f0ccb04d4408ae259d3aba3ae
Jeroen Nederhoed <jeroen@nederhoed.net>
2020-02-28 10:30:43 +0100

    cleaned up code in script.js and tested chooseRandomWord()

---
+++ test.test.js
@@ -0,0 +1,16 @@
+const chooseRandomWord = require('./script.js');
+
+// test('verify random word picker', function() {
+// 	const expected = /vis|toeter|developer|telefoon|moeder|snoer|geeuw/;
+
+// 	const output = chooseRandomWord([
+// 		'vis',
+// 		'toeter',
+// 		'developer',
+// 		'telefoon',
+// 		'moeder',
+// 		'snoer',
+// 		'geeuw'
+// 	]);
+// 	expect(output).toMatch(expected);
+// });
